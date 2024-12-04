b80f73f27ff09c9f938a8129e5fcd62174db7649
khushal655 <khushaly2207@gmail.com>
2024-12-04 15:56:29 +0530

    day8

---
+++ khushal day8/index.html/index.js
@@ -0,0 +1,59 @@
+// console.log("-----Welcome in JS-----");
+// let arr = [1,2,3,4,5,6,7,"hllo", true];
+// for( let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.push("shyam");
+// console.log(".................");
+// arr.pop();
+
+// // arr.splice(start,count,addon)
+// arr.splice(2, 1);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// const newArr = [5,5,6,4,3,5,4,5,4,3,5,5,5];
+// let count = 0;
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if (arr[i] === 5) {arr.slice(i, 1);
+
+//     }
+// }
+// console.log(arr);
+// const newArr = [2,4,5,8,8,6,5,4,4,4,5,6,7,7,7,]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if(arr[i] % 2 === 1) {
+//         sum += arr[i];
+//     }
+
+
+// }
+let student_id_card = {
+    Name : "Khushal",
+    Rollnumber : "244015",
+    Address : "shikohabad Uttar Pradesh",
+    Section : "A",
+    Course : "B.Tech",
+    branch : "ETCE",
+}
+console.log(student_id_card);
+const Name = student_id_card.Name;
+const Rollnumber = student_id_card.Rollnumber;
+const Address = student_id_card.Address;
+const Section = student_id_card.Section;
+const Course = student_id_card.Course;
+const branch = student_id_card.branch;
+
+console.log(Name);
+console.log(Rollnumber);
+console.log(Address);
+console.log(Section);
+console.log(Course);
+console.log(branch);
+
+console.log(student_id_card);
